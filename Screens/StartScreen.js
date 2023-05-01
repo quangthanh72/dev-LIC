67c7858a3517905098f4bc664bc26e49fabb3e12
quangthanh72 <thanhquangk22@gmail.com>
2023-05-01 13:50:13 +0700

    Add image

--- Screens/StartScreen.js
+++ Screens/StartScreen.js
@@ -1,12 +1,18 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
-import { View, Text , StyleSheet, SafeAreaView, StatusBar} from 'react-native'
+import { View, Text ,Image, StyleSheet, SafeAreaView, StatusBar} from 'react-native'
 import React from 'react'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+
 
 const StartScreen = () => {
   return (
+    <View  style={styles.container}>
     <SafeAreaView style = {{flex:1, marginTop: StatusBar.currentHeight}}>
-      <Text>StartScreen</Text>
+      <Image 
+      source={require('Q:\WorkSpace\library-code\dev-LIC\assets\Logo.png')}
+      />
     </SafeAreaView >
+    </View>
   )
 }
 
@@ -14,7 +20,7 @@ const StartScreen = () => {
 
 const styles =StyleSheet.create({
     container: {
-        backgroundColor: '6368D0',
+        backgroundColor: '#555ABF',
         flex: 1,
     },
 })
